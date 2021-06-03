ce7d382cc27e4f18e1326731438610acbf27ac1c
bachvu2 <85206533+bachvu2@users.noreply.github.com>
2021-06-03 21:08:24 +0700

    hello new

--- src/container/Album/AlbumList.tsx
+++ src/container/Album/AlbumList.tsx
@@ -10,14 +10,13 @@ import Album from './Album';
 class AlbumList extends Component<{ albums: any }> {
 
   render() {
-    // const Albums = []
     console.log("xxxxxxxx", this.props.albums)
     return (
       <div>
-        {/* {this.props.albums.map(e => {
+        {this.props.albums.map(e => {
           <Album key={e.id} album={e} />
         })
-        } */}
+        }
       </div>
     )
   }
